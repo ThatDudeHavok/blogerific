5f916e398874546ac4dda64106b6e0f1b6468151
Nicholas J. Olson <email": null>
2016-08-21 20:02:30 -0500

    Add files mistakenly left out of last commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  def create
+    @entry = Entry.find(params[:entry_id])
+    @comment = @entry.comments.create(comment_params)
+    redirect_to entry_path(@entry)
+  end
+
+  def destroy
+    @entry = Entry.find(params[:entry_id])
+    @comment = @entry.comments.find(params[:id])
+    @comment.destroy
+    redirect_to entry_path(@entry)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commentor, :body)
+    end
+end
